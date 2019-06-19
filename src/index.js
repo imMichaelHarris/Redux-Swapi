05f27a4cd5e4f040f30e5527aacc0a9d602867b4
Michael Harris <mhguitar1997@gmail.com>
2019-06-19 12:31:56 -0700

    Imported applyMiddleware and thunk in index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
+import thunk from 'redux-thunk',
+
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
